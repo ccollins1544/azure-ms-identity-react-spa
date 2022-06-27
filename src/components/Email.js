79d7c5f93e61c6e210a14db017c70c39f3f4049c
Christopher Collins <chris@ccollins.io>
2022-06-27 09:47:48 -0600

    added updateEmail function that is used to flag messages

--- src/components/Email.js
+++ src/components/Email.js
@@ -5,7 +5,7 @@ import { AuthenticatedTemplate } from '@azure/msal-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import moment from 'moment';
-import { getUserEmails, deleteEmail } from '../GraphService';
+import { getUserEmails, deleteEmail, updateEmail } from '../GraphService';
 import { useAppContext } from '../AppContext';
 import Debug from "./Debug";
 
@@ -43,6 +43,54 @@ const Email = (props) => {
     return;
   };
 
+  const flagMessage = async (event) => {
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    let clickedElement = event?.target || {};
+    let { dataset, tagName, parentElement } = clickedElement || {};
+
+    // Go up one level in the DOM 
+    if (tagName !== "svg" && parentElement?.tagName === "svg") {
+      clickedElement = parentElement;
+      parentElement = clickedElement.parentElement;
+      dataset = { ...dataset, ...clickedElement?.dataset };
+      tagName = clickedElement?.tagName;
+    }
+
+    let { id: emailId, flag_status } = dataset || {};
+
+    /**
+     * Flag Status Flow:
+     * flagged -> complete
+     * notFlagged -> Flagged
+     * complete -> notFlagged
+     */
+    let flagStatus = "notFlagged";
+    if (flag_status === "flagged") {
+      flagStatus = "complete";
+    } else if (flag_status === "notFlagged") {
+      flagStatus = "flagged";
+    }
+    let updatedEmail = {
+      flag: {
+        flagStatus,
+      }
+    };
+
+    try {
+      if (!emailId || /\D/.test(emailId) === false) {
+        throw new Error(`Invalid ID ${emailId}`);
+      }
+      await updateEmail(authProvider, emailId, updatedEmail);
+      setInvokeUpdate((prev) => !prev);
+    } catch (err) {
+      displayError(err.message);
+    }
+    return;
+  };
+
   useEffect(() => {
     const loadEmails = async () => {
       if (user) {
@@ -69,6 +117,7 @@ const Email = (props) => {
           {Array.isArray(emails) && emails.length > 0 && <Table size="sm">
             <thead>
               <tr>
+                <th>Flag</th>
                 <th>Read</th>
                 <th>From</th>
                 <th>Received</th>
@@ -78,7 +127,8 @@ const Email = (props) => {
             </thead>
             <tbody>
               {emails.map(email => <tr key={email.id}>
-                <td><FontAwesomeIcon icon={email.isRead ? solid('trash') : solid('trash-can')} onClick={deleteMessage} data-id={email.id} /></td>
+                <td><FontAwesomeIcon icon={email.flag?.flagStatus === "flagged" ? solid('flag') : (email.flag?.flagStatus === "complete" ? solid('flag-checkered') : solid('font-awesome'))} data-id={email.id} data-flag_status={email.flag?.flagStatus} onClick={flagMessage} /></td>
+                <td><FontAwesomeIcon icon={email.isRead ? solid('trash') : solid('trash-can')} data-id={email.id} onClick={deleteMessage} /></td>
                 <td>{email.from.emailAddress.name}</td>
                 <td>{email.receivedDateTime ? moment(email.receivedDateTime).format('MM/DD/YYYY hh:mm A') : ''}</td>
                 <td>{email.subject}</td>
